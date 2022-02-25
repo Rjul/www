f23afab543001a82f78758814d1a630251715df8
Hummel julien <hummel.julien.pro@gmail.com>
2022-02-25 13:39:16 +0000

    laravel mix fix

--- html/webpack.mix.js
+++ html/webpack.mix.js
@@ -17,4 +17,4 @@ mix.js('resources/js/app.js', 'public/js')
     ]);
 
 mix.js("vendor/orchid/platform/public/js", "public/resources/orchid/js")
-    .postCss("/vendor/orchid/platform/public/css", "public/resources/orchid/css");
+    .postCss("vendor/orchid/platform/public/css", "public/resources/orchid/css");
